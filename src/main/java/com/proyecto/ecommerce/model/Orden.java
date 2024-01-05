20cc637fbfdf6e17cfa1e8c7002cf9233480c817
Matias Criollo <matiascriollo57@gmail.com>
2024-01-05 18:08:57 -0500

    Mapeo de clases a tablas y relaciones con JPA

--- src/main/java/com/proyecto/ecommerce/model/Orden.java
+++ src/main/java/com/proyecto/ecommerce/model/Orden.java
@@ -1,14 +1,26 @@
 package com.proyecto.ecommerce.model;
 
+import jakarta.persistence.*;
+
 import java.util.Date;
 
+@Entity
+@Table(name = "ordenes")
 public class Orden {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String numero;
     private Date fechaCreacion;
     private Date fechaRecibido;
     private double total;
+
+    @ManyToOne
+    private Usuario usuario;
+
+    @OneToOne(mappedBy = "orden")
+    private DetalleOrden detalle;
 
     public Orden() {
     }
@@ -54,6 +66,20 @@ public class Orden {
     }
     public void setTotal(double total) {
         this.total = total;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public DetalleOrden getDetalle() {
+        return detalle;
+    }
+    public void setDetalle(DetalleOrden detalle) {
+        this.detalle = detalle;
     }
 
     @Override
