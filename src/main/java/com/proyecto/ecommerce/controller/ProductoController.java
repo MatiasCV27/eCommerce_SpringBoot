005520174fb12fa216f7c70ef81dfff3e6afa522
Matias Criollo <matiascriollo57@gmail.com>
2024-01-06 12:26:19 -0500

    Agregando las funcionalidades CRUD para los productos

--- src/main/java/com/proyecto/ecommerce/controller/ProductoController.java
+++ src/main/java/com/proyecto/ecommerce/controller/ProductoController.java
@@ -1,13 +1,19 @@
 package com.proyecto.ecommerce.controller;
 
 import com.proyecto.ecommerce.model.Producto;
+import com.proyecto.ecommerce.model.Usuario;
+import com.proyecto.ecommerce.service.ProductoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/productos")
@@ -15,8 +21,12 @@ public class ProductoController {
 
     private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
 
+    @Autowired
+    private ProductoService productoService;
+
     @GetMapping("")
-    public String show() {
+    public String show(Model model) {
+        model.addAttribute("productos", productoService.findAll());
         return "productos/show";
     }
 
@@ -28,7 +38,33 @@ public class ProductoController {
     @PostMapping("/save")
     public String save(Producto producto) {
         LOGGER.info("Este es el objeto producto {}", producto);
+        Usuario u = new Usuario(2, "", "", "", "", "", "", "");
+        producto.setUsuario(u);
+        productoService.save(producto);
         return "redirect:/productos";
     }
 
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+        Producto producto = new Producto();
+        Optional<Producto> optionalProducto = productoService.get(id);
+        producto = optionalProducto.get();
+
+        LOGGER.info("Producto buscado: {}", producto);
+        model.addAttribute("producto", producto);
+
+        return "productos/edit";
+    }
+
+    @PostMapping("/update")
+    public String update(Producto producto) {
+        productoService.update(producto);
+        return "redirect:/productos";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Integer id) {
+        productoService.delete(id);
+        return "redirect:/productos";
+    }
 }
