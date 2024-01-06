b83e5925e71eb30c60a3801b642b4719e0552d15
Matias Criollo <matiascriollo57@gmail.com>
2024-01-06 18:46:27 -0500

    Mostrando las imagenes en el Home

--- src/main/java/com/proyecto/ecommerce/controller/ProductoController.java
+++ src/main/java/com/proyecto/ecommerce/controller/ProductoController.java
@@ -69,18 +69,18 @@ public class ProductoController {
     @PostMapping("/update")
     public String update(Producto producto, @RequestParam("img") MultipartFile file) throws IOException {
 
+        Producto p = new Producto();
+        p = productoService.get(producto.getId()).get();
+
         if (file.isEmpty()) { // Editamos el producto pero no cambiamos la imagen
-            Producto p = new Producto();
-            p = productoService.get(producto.getId()).get();
             producto.setImagen(p.getImagen());
         } else { // Cuando se edita la imagen
-            Producto p = new Producto();
-            p = productoService.get(producto.getId()).get();
             // Eliminar cuando no sea la imagen por defecto
             if (!p.getImagen().equals("default.jpg")) upload.deleteImage(p.getImagen());
             String nombreImagen = upload.saveImage(file);
             producto.setImagen(nombreImagen);
         }
+        producto.setUsuario(p.getUsuario());
 
         productoService.update(producto);
         return "redirect:/productos";
