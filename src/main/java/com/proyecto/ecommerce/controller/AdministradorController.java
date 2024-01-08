05af527cc049abb0829a653a54be4d34469c2a8b
Matias Criollo <matiascriollo57@gmail.com>
2024-01-08 12:54:40 -0500

    Ver las ordenes y detalles de una orden en el sistema de administración

--- src/main/java/com/proyecto/ecommerce/controller/AdministradorController.java
+++ src/main/java/com/proyecto/ecommerce/controller/AdministradorController.java
@@ -1,12 +1,17 @@
 package com.proyecto.ecommerce.controller;
 
+import com.proyecto.ecommerce.model.Orden;
 import com.proyecto.ecommerce.model.Producto;
+import com.proyecto.ecommerce.service.IOrdenService;
 import com.proyecto.ecommerce.service.IProductoService;
 import com.proyecto.ecommerce.service.IUsuarioService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -15,11 +20,16 @@ import java.util.List;
 @RequestMapping("/administrador")
 public class AdministradorController {
 
+    private final Logger log = LoggerFactory.getLogger(AdministradorController.class);
+
     @Autowired
     private IProductoService IProductoService;
 
     @Autowired
     private IUsuarioService usuarioService;
+
+    @Autowired
+    private IOrdenService ordenService;
 
     @GetMapping("")
     public String home(Model model) {
@@ -34,6 +44,22 @@ public class AdministradorController {
     public String usuarios(Model model) {
         model.addAttribute("usuarios", usuarioService.findAll());
         return "administrador/usuarios";
+    }
+
+    @GetMapping("/ordenes")
+    public String ordenes(Model model) {
+        model.addAttribute("ordenes", ordenService.findAll());
+        return "administrador/ordenes";
+    }
+
+    @GetMapping("/detalle/{id}")
+    public String detalle(Model model, @PathVariable Integer id) {
+        log.info("Id de la orden: {}", id);
+
+        Orden orden = ordenService.findById(id).get();
+        model.addAttribute("detalles", orden.getDetalle());
+
+        return "administrador/detalleorden";
     }
 
 }
