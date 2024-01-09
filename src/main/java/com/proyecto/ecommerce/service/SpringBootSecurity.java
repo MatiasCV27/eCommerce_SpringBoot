0f40bb431154e1f99fac7a16ed8035f08d419ac8
Matias Criollo <matiascriollo57@gmail.com>
2024-01-08 23:32:03 -0500

    Actualizacion parcial del SecurityConfig

--- src/main/java/com/proyecto/ecommerce/service/SpringBootSecurity.java
+++ src/main/java/com/proyecto/ecommerce/service/SpringBootSecurity.java
@@ -3,38 +3,49 @@ package com.proyecto.ecommerce.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.SecurityBuilder;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-public class SpringBootSecurity extends WebSecurityConfigurerAdapter {
+public class SpringBootSecurity implements WebSecurityConfigurer {
 
     @Autowired
-    private UserDetailsService userDetailService;
-
+    private UserDetailsService userDetailsService;
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailService).passwordEncoder(getEnecoder());
+    public void init(SecurityBuilder builder) throws Exception {}
+
+    @Override
+    public void configure(SecurityBuilder builder) throws Exception {
+        HttpSecurity http = builder.http();
+
+        configurarAutenticación(http);
+        configurarAutorización(http);
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests()
+    private void configurarAutenticación(HttpSecurity http) throws Exception {
+        http.formLogin(Customizer.withDefaults())
+                .loginPage("/usuario/login")
+                .permitAll()
+                .defaultSuccessUrl("/usuario/acceder");
+    }
+
+    private void configurarAutorización(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
                 .antMatchers("/administrador/**").hasRole("ADMIN")
                 .antMatchers("/productos/**").hasRole("ADMIN")
-                .and().formLogin().loginPage("/usuario/login")
-                .permitAll().defaultSuccessUrl("/usuario/acceder");
+                .anyRequest().authenticated()  // Requerir autenticación para todas las demás solicitudes
+        ;
     }
 
     @Bean
-    public BCryptPasswordEncoder getEnecoder() {
+    public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
 }
